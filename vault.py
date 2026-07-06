6845cea8ab92bcd3467e28fe27fd40841cefd56f
Ananya V <ananyav2076@gmail.com>
2026-07-06 20:08:50 +0530

    Update vault.py
    completed the main file of this project

--- vault.py
+++ vault.py
@@ -13,9 +13,9 @@ stored_hash, stored_salt = db.get_master_password()
 # if the user is using PyVault for the first time - set the password
 if stored_hash == None and stored_salt == None:
     # getpass shows '*' while typing - Password Masking (stars)
-    password = getpass("Set Master Password: ")
+    password = getpass("> Set Master Password: ")
     # asking user to confirm the password
-    confirm =  getpass("Confirm Master Password: ")
+    confirm =  getpass("> Confirm Master Password: ")
     if password != confirm:
         print("Passwords don't match!")
         exit()
@@ -25,7 +25,7 @@ if stored_hash == None and stored_salt == None:
     stored_hash, stored_salt = hash, salt
 
 # every time the user has to enter the master password to use PyVault
-password_verify = getpass("Enter Master Password: ")
+password_verify = getpass("> Enter Master Password: ")
 status = auth.verify_master_password(password_verify, stored_hash,stored_salt)
 
 if status == True:
@@ -33,10 +33,11 @@ if status == True:
     # key generated using salt and master password (not the hash)
     key = enc.generate_key(password_verify, stored_salt)
     while True:
-        print("What do u want to do?")
+        print("> What do u want to do?")
         print("1. Add Password\n2. Get Password\n3. List all Sites\n4.Delete Entry\n5. Exit")
         choice = int(input("\n> "))
 
+        # add password 
         if choice == 1:
             print("> Enter the following details...")
             site = input("> Site: ")
@@ -46,10 +47,11 @@ if status == True:
             db.add_site_password(site, username, encrypted_password)
             print("Password Encrypted and Saved!")
 
+        # get password 
         elif choice == 2:
             site = input("> Enter the site: ")
             username, password = db.get_site_password(site)
-            # if the site does not exist
+            # check if site exists or not
             if username == None: 
                 print("Site Not Found!")
             else: 
@@ -57,18 +59,40 @@ if status == True:
                 print("Username: ", username)
                 print("Password: ", decrypted_password)
 
+        # list all sites
         elif choice == 3:
             rows = db.list_sites()
-            for site, username in rows:
-                print("Site: ", site) 
-                print("Username: ", username)
-                print(" ******************** ")
+            # check if any passwords saved yet
+            if rows == []:
+                print("No passwords saved yet!")
+            else:
+                # Tuple Unpacking 
+                for site, username in rows:
+                    print("Site: ", site) 
+                    print("Username: ", username)
+                    print(" ******************** ")
+        
+        # delete entry
+        elif choice == 4:
+            site = input("> Enter the site you want to delete: ")
+            # check if site exists or not
+            username, _ = db.get_site_password(site)  # The '_' is a Python convention for "I don't need this value" 
+            if username == None:
+                print("Site Not Found!")
+            else:
+                db.delete_site_password(site)
+                print("Entry deleted successfully!")
 
+        # exit
+        elif choice == 5:
+            print("THANK YOU!")
+            exit()
+
+        else:
+            print("Invalid choice, choose again...")
 
 else:
     print("Wrong Master Password!")
-    exit()  # the program stops
-
 
 
 
