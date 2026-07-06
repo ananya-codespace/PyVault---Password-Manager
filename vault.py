c88da383213ba5439f8f046766d3068f8640d607
Ananya V <ananyav2076@gmail.com>
2026-07-06 14:57:03 +0530

    Update vault.py
    started writing the code for vault.py

--- vault.py
+++ vault.py
@@ -1,6 +1,37 @@
 # Main file (CLI menu, user interaction)
+import database as db
+import auth 
+import encryption as enc
+from getpass import getpass
 
+print("\n -------------------- PyVault -------------------- \n")
+print("WELCOME!\n")
+# table created, if not exists, as soon as the program runs
+db.init_db()
+# retrieving the stored master password and salt
+stored_hash, stored_salt = db.get_master_password()
+# if the user is using PyVault for the first time - set the password
+if stored_hash == None and stored_salt == None:
+    # getpass shows '*' while typing - Password Masking (stars)
+    password = getpass("Set Master Password: ")
+    # asking user to confirm the password
+    confirm =  getpass("Confirm Master Password: ")
+    if password != confirm:
+        print("Passwords don't match!")
+        exit()
+    hash, salt = auth.set_master_password(password)
+    db.save_master_password(hash, salt)
+    # db has the new values but still None in memory, hence manually updating the values, or will have to call get_master_password()
+    stored_hash, stored_salt = hash, salt
 
+# every time the user has to enter the master password to use PyVault
+password_verify = getpass("Enter Master Password: ")
+status = auth.verify_master_password(password_verify, stored_hash,stored_salt)
+if status == True:
+    print("Unlocked!")
+else:
+    print("Wrong Master Password!")
+    exit()  # the program stops
 
 
 
